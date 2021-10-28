7e6872af825a5886944ae37d07ff4b5cc6b6feec
BitcoinIsCash <tangoswapcash@gmail.cash>
2021-10-28 13:22:24 -0300

    MLP -> TLP

--- contracts/uniswapv2/UniswapV2ERC20.sol
+++ contracts/uniswapv2/UniswapV2ERC20.sol
@@ -8,7 +8,7 @@ contract UniswapV2ERC20 {
     using SafeMathUniswap for uint;
 
     string public constant name = 'TANGOswap LP Token';
-    string public constant symbol = 'MLP';
+    string public constant symbol = 'TLP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
