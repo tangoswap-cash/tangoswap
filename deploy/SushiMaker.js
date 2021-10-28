748d6cf5d700ef75b49556ac34aa890a5b965eb3
BitcoinIsCash <tangoswapcash@gmail.cash>
2021-10-28 13:23:37 -0300

    WETH9 -> WBCH

--- deploy/SushiMaker.js
+++ deploy/SushiMaker.js
@@ -1,4 +1,6 @@
-const { WETH9 } = require("@tangoswapcash/sdk")
+const { WBCH } = require("@tangoswapcash/sdk")
+
+console.log("WBCH: ", WBCH);
 
 module.exports = async function ({ ethers: { getNamedSigner }, getNamedAccounts, deployments }) {
   const { deploy } = deployments
@@ -15,8 +17,8 @@ module.exports = async function ({ ethers: { getNamedSigner }, getNamedAccounts,
   
   if (chainId === '31337') {
     wethAddress = (await deployments.get("WETH9Mock")).address
-  } else if (chainId in WETH9) {
-    wethAddress = WETH9[chainId].address
+  } else if (chainId in WBCH) {
+    wethAddress = WBCH[chainId].address
   } else {
     throw Error("No WETH!")
   }
