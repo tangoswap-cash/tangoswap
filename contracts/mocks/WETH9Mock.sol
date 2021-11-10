cf3ec54a77db429f42602bb7489abfc76594fd9f
BitcoinIsCash <tangoswapcash@gmail.cash>
2021-11-10 17:55:38 -0300

    new version of WETH9Mock

--- contracts/mocks/WETH9Mock.sol
+++ contracts/mocks/WETH9Mock.sol
@@ -2,62 +2,29 @@
 
 pragma solidity 0.6.12;
 
-contract WETH9Mock {
-	string public name = "Wrapped Ether";
-	string public symbol = "WETH";
-	uint8 public decimals = 18;
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ERC20Public.sol";
+import '../uniswapv2/interfaces/IWETH.sol';
 
-	event Approval(address indexed src, address indexed guy, uint256 wad);
-	event Transfer(address indexed src, address indexed dst, uint256 wad);
+// import "hardhat/console.sol";   //TODO(BiC) remove
+
+contract WETH9Mock is ERC20Public { //, IWETH
+	string public k_name = "Wrapped Ether";
+	string public k_symbol = "WETH";
+
 	event Deposit(address indexed dst, uint256 wad);
-	event Withdrawal(address indexed src, uint256 wad);
 
-	mapping(address => uint256) public balanceOf;
-	mapping(address => mapping(address => uint256)) public allowance;
+    constructor(
+        uint256 supply
+    ) public ERC20Public(k_name, k_symbol) {
+        _mint(msg.sender, supply);
+    }
 
 	function deposit() public payable {
-		balanceOf[msg.sender] += msg.value;
+		// console.log("contract WETH9Mock.deposit() 1");
+		_balances[msg.sender] += msg.value;
+		// console.log("contract WETH9Mock.deposit() 2");
 		emit Deposit(msg.sender, msg.value);
-	}
-
-	function withdraw(uint256 wad) public {
-		require(balanceOf[msg.sender] >= wad, "WETH9: Error");
-		balanceOf[msg.sender] -= wad;
-		msg.sender.transfer(wad);
-		emit Withdrawal(msg.sender, wad);
-	}
-
-	function totalSupply() public view returns (uint256) {
-		return address(this).balance;
-	}
-
-	function approve(address guy, uint256 wad) public returns (bool) {
-		allowance[msg.sender][guy] = wad;
-		emit Approval(msg.sender, guy, wad);
-		return true;
-	}
-
-	function transfer(address dst, uint256 wad) public returns (bool) {
-		return transferFrom(msg.sender, dst, wad);
-	}
-
-	function transferFrom(
-		address src,
-		address dst,
-		uint256 wad
-	) public returns (bool) {
-		require(balanceOf[src] >= wad, "WETH9: Error");
-
-		if (src != msg.sender && allowance[src][msg.sender] != uint256(-1)) {
-			require(allowance[src][msg.sender] >= wad, "WETH9: Error");
-			allowance[src][msg.sender] -= wad;
-		}
-
-		balanceOf[src] -= wad;
-		balanceOf[dst] += wad;
-
-		emit Transfer(src, dst, wad);
-
-		return true;
+		// console.log("contract WETH9Mock.deposit() 3");
 	}
 }
