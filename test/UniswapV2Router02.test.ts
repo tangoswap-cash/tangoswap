d77ced6b2bd26169ff8340e33475a97787ec7751
BitcoinIsCash <tangoswapcash@gmail.cash>
2021-11-10 18:16:16 -0300

    tests

--- test/UniswapV2Router02.test.ts
+++ test/UniswapV2Router02.test.ts
@@ -4,41 +4,52 @@ const { ethers: { constants: { MaxUint256 }}} = require("ethers")
 
 describe("UniswapV2Router02", function () {
   before(async function () {
-    await prepare(this, ["UniswapV2Router02", "ERC20Mock", "UniswapV2Factory", "UniswapV2Pair"])
+    await prepare(this, ["UniswapV2Router02", "ERC20Mock", "UniswapV2Factory", "UniswapV2Pair", "WETH9Mock"])
   })
 
   beforeEach(async function () {
     await deploy(this, [
-      ["sushi", this.ERC20Mock, ["SUSHI", "SUSHI", getBigNumber("10000000")]],
-      ["dai", this.ERC20Mock, ["DAI", "DAI", getBigNumber("10000000")]],
-      ["mic", this.ERC20Mock, ["MIC", "MIC", getBigNumber("10000000")]],
-      ["usdc", this.ERC20Mock, ["USDC", "USDC", getBigNumber("10000000")]],
-      ["weth", this.ERC20Mock, ["WETH", "ETH", getBigNumber("10000000")]],
-      ["strudel", this.ERC20Mock, ["$TRDL", "$TRDL", getBigNumber("10000000")]],
+      ["sushi", this.ERC20Mock, ["SUSHI", "SUSHI", getBigNumber("5000000000000000000")]],
+      // ["dai", this.ERC20Mock, ["DAI", "DAI", getBigNumber("10000000")]],
+      // ["mic", this.ERC20Mock, ["MIC", "MIC", getBigNumber("10000000")]],
+      // ["usdc", this.ERC20Mock, ["USDC", "USDC", getBigNumber("10000000")]],
+      // ["weth", this.WETH9Mock, [getBigNumber("10000000")]],
+      ["weth", this.WETH9Mock, [getBigNumber("54800000000000000")]],
+      // ["strudel", this.ERC20Mock, ["$TRDL", "$TRDL", getBigNumber("10000000")]],
+
       ["factory", this.UniswapV2Factory, [this.alice.address]],
     ])
 
     await deploy(this, [["router", this.UniswapV2Router02, [this.factory.address, this.weth.address]]])
-    // await deploy(this, [["bar", this.SushiBar, [this.sushi.address]]])
-    // await deploy(this, [["sushiMaker", this.SushiMaker, [this.factory.address, this.bar.address, this.sushi.address, this.weth.address]]])
-    // await deploy(this, [["exploiter", this.SushiMakerExploitMock, [this.sushiMaker.address]]])
+
     await createSLP(this, "sushiEth", this.sushi, this.weth, getBigNumber(10))
-    await createSLP(this, "strudelEth", this.strudel, this.weth, getBigNumber(10))
-    await createSLP(this, "daiEth", this.dai, this.weth, getBigNumber(10))
-    await createSLP(this, "usdcEth", this.usdc, this.weth, getBigNumber(10))
-    await createSLP(this, "micUSDC", this.mic, this.usdc, getBigNumber(10))
-    await createSLP(this, "sushiUSDC", this.sushi, this.usdc, getBigNumber(10))
-    await createSLP(this, "daiUSDC", this.dai, this.usdc, getBigNumber(10))
-    await createSLP(this, "daiMIC", this.dai, this.mic, getBigNumber(10))
+    // await createSLP(this, "strudelEth", this.strudel, this.weth, getBigNumber(10))
+    // await createSLP(this, "daiEth", this.dai, this.weth, getBigNumber(10))
+    // await createSLP(this, "usdcEth", this.usdc, this.weth, getBigNumber(10))
+    // await createSLP(this, "micUSDC", this.mic, this.usdc, getBigNumber(10))
+    // await createSLP(this, "sushiUSDC", this.sushi, this.usdc, getBigNumber(10))
+    // await createSLP(this, "daiUSDC", this.dai, this.usdc, getBigNumber(10))
+    // await createSLP(this, "daiMIC", this.dai, this.mic, getBigNumber(10))
   })
+
   describe("addLiquidityETH", function () {
     it("tests addLiquidityETH", async function () {
+
+      // // console.log("this.weth.constructor.name: ", this.weth.constructor.name);
+      // console.log("this.weth: ", this.weth);
+
+      // const weth2 = await this.WETH9Mock.deploy();
+      // await weth2.deployed();
+
+      // console.log("weth2: ", weth2);
+
+
       const overrides = {
         gasLimit: 9500000
       }
 
-      const sushiAmount = "1";
-      const WETHAmount = "1";
+      const sushiAmount = "813666000000000000";
+      const WETHAmount = "1000000000000000";
 
       console.log("test addLiquidityETH this.router.address:   ", this.router.address);
       console.log("test addLiquidityETH this.sushi.address:    ", this.sushi.address);
