b0c8d46326694966338bbf62cb6b8ad9db4a9391
BitcoinIsCash <tangoswapcash@gmail.cash>
2021-11-21 09:16:34 -0300

    misc

--- contracts/SushiToken.sol
+++ contracts/SushiToken.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // WARNING: There is a known vuln contained within this contract related to vote delegation,
 // it's NOT recommmended to use this in production.
 
-
 // SushiToken with Governance.
 contract SushiToken is ERC20("TangoToken", "TANGO"), Ownable {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
